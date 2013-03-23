53144a21dc432c33434ed85a8ebe65d5bd99103a
mjervis <mjervis@gmail.com>
2013-03-23 13:05:27 +0000

    First commit

---
+++ Strava Centurion/Properties/AssemblyInfo.cs
@@ -0,0 +1,46 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="FuckingBrit.com">
+//   Copyright (c) 2013 FuckingBrit.com
+//   Source code available under a total unrestrictive, free, it's all yours licence.
+//   Use at your own risk.
+// </copyright>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Strava Centurion")]
+[assembly: AssemblyDescription("Power Estimate Calculator for Garmin TPX Files.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FuckingBrit.com")]
+[assembly: AssemblyProduct("Strava Centurion")]
+[assembly: AssemblyCopyright("Copyright © FuckingBrit.com 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("419ac239-b3a8-47f7-a07a-34e95e355de1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
